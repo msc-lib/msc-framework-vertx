41bd9930035a4c7c2ca85fb0e1ae749fd196b34d
13011009495 <gaojingzhe@yunzujia.com>
2018-08-27 17:13:40 +0800

    add resteasy json provider

--- src/main/java/fun/pplm/msc/framework/vertx/ResteasyVerticle.java
+++ src/main/java/fun/pplm/msc/framework/vertx/ResteasyVerticle.java
@@ -1,5 +1,9 @@
 package fun.pplm.msc.framework.vertx;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jboss.resteasy.plugins.providers.jackson.ResteasyJackson2Provider;
 import org.jboss.resteasy.plugins.server.vertx.VertxRegistry;
 import org.jboss.resteasy.plugins.server.vertx.VertxRequestHandler;
 import org.jboss.resteasy.plugins.server.vertx.VertxResteasyDeployment;
@@ -10,23 +14,31 @@ public abstract class ResteasyVerticle extends AbstractVerticle {
 	
 	public static final String CONFIG_KEY_PORT = "RESTEASY_VERTICLE_PORT";
 	
+	@SuppressWarnings("rawtypes")
 	@Override
 	public void start() throws Exception {
 		VertxResteasyDeployment deployment = new VertxResteasyDeployment();
-		deploy(deployment);
+		
+		List<Class<?>> classes = new ArrayList<>();
+		registerProviders(classes);
+		if (!classes.contains(ResteasyJackson2Provider.class)) {
+			classes.add(ResteasyJackson2Provider.class);
+		}
+		
+		deployment.setActualProviderClasses(new ArrayList<Class>(classes));
 	    deployment.start();
 	    VertxRegistry registry = deployment.getRegistry();
-	    register(registry);
+	    registerService(registry);
 
 	    vertx.createHttpServer()
 	        .requestHandler(new VertxRequestHandler(vertx, deployment))
 	        .listen(Integer.parseInt(System.getProperty(CONFIG_KEY_PORT, "8080")), ar -> {
-	          System.out.println("Server started on port "+ ar.result().actualPort());
+	        	System.out.println("Server started on port "+ ar.result().actualPort());
 	        });
 	}
 	
-	protected abstract void deploy(VertxResteasyDeployment deployment);
+	protected abstract void registerProviders(List<Class<?>> classes);
 	
-	protected abstract void register(VertxRegistry registry); 
+	protected abstract void registerService(VertxRegistry registry); 
 	
 }
